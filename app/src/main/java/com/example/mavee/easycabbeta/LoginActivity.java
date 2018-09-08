ef2093801f438cb568601470694859c72869c7de
EasyCab <42908726+EasyCab@users.noreply.github.com>
2018-09-08 13:21:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mavee/easycabbeta/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.mavee.easycabbeta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,EditProfileActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
