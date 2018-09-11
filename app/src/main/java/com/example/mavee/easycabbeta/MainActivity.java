4e8f9beac0019ea9c4d9c847bf679c831d99e07e
EasyCab <42908726+EasyCab@users.noreply.github.com>
2018-09-12 00:12:18 +0500

    ccp imported

--- app/src/main/java/com/example/mavee/easycabbeta/MainActivity.java
+++ app/src/main/java/com/example/mavee/easycabbeta/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView socialLink;
+    private Button  sendCode;
 
 
     @Override
@@ -16,12 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sendCode=(Button) findViewById(R.id.btn_send_code);
         socialLink = (TextView ) findViewById(R.id.social_link);
 
         socialLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        sendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,VerificationActivity.class);
                 startActivity(i);
             }
         });
