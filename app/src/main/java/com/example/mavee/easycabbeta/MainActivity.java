efd4aae58e5133e78e196a1a8975b2c5133c9d07
EasyCab <42908726+EasyCab@users.noreply.github.com>
2018-09-12 22:43:02 +0500

    phone verification enabled

--- app/src/main/java/com/example/mavee/easycabbeta/MainActivity.java
+++ app/src/main/java/com/example/mavee/easycabbeta/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView socialLink;
     private Button  sendCode;
+    private EditText askPhone;
 
 
     @Override
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        askPhone=(EditText)findViewById(R.id.ask_phone);
         sendCode=(Button) findViewById(R.id.btn_send_code);
         socialLink = (TextView ) findViewById(R.id.social_link);
 
@@ -32,7 +35,17 @@ public class MainActivity extends AppCompatActivity {
         sendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String mobile = askPhone.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    askPhone.setError("Enter a valid mobile");
+                    askPhone.requestFocus();
+                    return;
+                }
+
                 Intent i = new Intent(MainActivity.this,VerificationActivity.class);
+                i.putExtra("mobile",mobile);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/mavee/easycabbeta/VerificationActivity.java
+++ app/src/main/java/com/example/mavee/easycabbeta/VerificationActivity.java
@@ -1,13 +1,33 @@
 package com.example.mavee.easycabbeta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerificationActivity extends AppCompatActivity {
+
+
+    private Button btn_verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+
+        String phonenumber = getIntent().getStringExtra("mobile");
+    btn_verify=(Button) findViewById(R.id.btn_verify);
+
+    btn_verify.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent i = new Intent(VerificationActivity.this,EditProfileActivity.class);
+            startActivity(i);
+        }
+    });
+
     }
 }
